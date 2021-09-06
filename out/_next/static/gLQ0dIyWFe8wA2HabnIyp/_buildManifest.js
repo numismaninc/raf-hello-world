adbb1e32cdbf10e8b81513d65cfa70893748a93d
Scott Cromie <scromie_cm@aspirehealthcare.com>
2021-09-06 11:05:09 -0600

    Deploy Next.js to gh-pages

---
+++ out/_next/static/gLQ0dIyWFe8wA2HabnIyp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2fc886ed6e51ae7deacf.js"],"/_error":["static/chunks/pages/_error-eb7f79f2af83e8ddf5e2.js"],"/about":["static/chunks/pages/about-8d7b063a01bf3aceb58e.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
